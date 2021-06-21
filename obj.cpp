1d832ef07e0150ff316fd5aa84125d17642bc730
GrapeJuicer <Dev.Grape@outlook.jp>
2021-06-21 11:19:34 +0900

    [update] add class: Vec3

--- obj.cpp
+++ obj.cpp
@@ -1,2 +1,24 @@
 #include "obj.hpp"
 
+Vec3::Vec3()
+{
+    this->x = 0.0;
+    this->y = 0.0;
+    this->z = 0.0;
+}
+
+Vec3::Vec3(double x, double y, double z)
+{
+    this->x = x;
+    this->y = y;
+    this->z = z;
+}
+
+Vec3::~Vec3()
+{
+}
+
+double Vec3::length()
+{
+    return sqrt(std::pow(this->x, 2) + std::pow(this->y, 2) + std::pow(this->z, 2));
+}
--- obj.hpp
+++ obj.hpp
@@ -1,1 +1,15 @@
 #include <iostream>
+
+class Vec3
+{
+private:
+public:
+    double x;
+    double y;
+    double z;
+    Vec3();
+    Vec3(double x, double y, double z);
+    ~Vec3();
+    double length();
+};
+
