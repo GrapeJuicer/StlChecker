d0e191f57fec02d2c1bb025ffa3c999186589d1a
GrapeJuicer <Dev.Grape@outlook.jp>
2021-06-21 12:05:44 +0900

    [update] add class: Face

--- obj.cpp
+++ obj.cpp
@@ -19,3 +19,32 @@ double Vec3::length()
 {
     return sqrt(std::pow(this->x, 2) + std::pow(this->y, 2) + std::pow(this->z, 2));
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+Face::Face() : Face(Vec3(), Vec3(), Vec3(), Vec3())
+{
+}
+
+Face::Face(Vec3 p[3], Vec3 normal) : Face(p[0], p[1], p[2], normal)
+{
+}
+
+Face::Face(Vec3 p1, Vec3 p2, Vec3 p3, Vec3 normal)
+{
+    this->point[0] = p1;
+    this->point[1] = p2;
+    this->point[2] = p3;
+    this->normal = normal;
+}
+
+Face::Face(double x1, double y1, double z1,
+           double x2, double y2, double z2,
+           double x3, double y3, double z3,
+           double nx, double ny, double nz) : Face(Vec3(x1, y1, z1), Vec3(x2, y2, z2), Vec3(x3, y3, z3), Vec3(nx, ny, nz))
+{
+}
+
+Face::~Face()
+{
+}
--- obj.hpp
+++ obj.hpp
@@ -13,3 +13,16 @@ public:
     double length();
 };
 
+class Face
+{
+private:
+public:
+    Vec3 point[3];
+    Vec3 normal;
+    Face();
+    Face(Vec3 p[3], Vec3 normal);
+    Face(Vec3 p1, Vec3 p2, Vec3 p3, Vec3 normal);
+    Face(double x1, double y1, double z1, double x2, double y2, double z2, double x3, double y3, double z3, double nx, double ny, double nz);
+    ~Face();
+};
+
