3d7622bb82d5466f2436e6649f8cc3f1fdf868d9
GrapeJuicer <Dev.Grape@outlook.jp>
2021-07-17 22:09:39 +0900

    [update] add option

--- main.cpp
+++ main.cpp
@@ -1,41 +1,116 @@
 #include <iostream>
 #include "obj.hpp"
+#include "optarg.h"
+
+#define OPTSIZE 10
+
+#define VERSION "1.0.0"
+#define ISSUE_LINK "https://github.com/GrapeJuicer/StlChecker/issues"
 
 using namespace std;
 
+void printHelp();
+void printVersion();
+
+bool isShow = true;
+
 int main(int argc, char *argv[])
 {
-    if (argc != 4) // .exe file1 file2 source_type
-    {
-        cout << "Error: Invlid argumanets." << endl;
-        return -1;
-    }
-
-    string srctype = argv[argc - 1];
+    /*
+    help
+    version
+    format
+    result
+    */
+    char shortotps[] = "hvf:r";
+    struct option longopts[] = {
+        {"help", no_argument, NULL, 'h'},
+        {"version", no_argument, NULL, 'v'},
+        {"format", required_argument, NULL, 'f'},
+        {"result", no_argument, NULL, 'r'},
+        OPT_END};
+    struct optarg findopts[OPTSIZE];
+    int folen;
+    string srctype = "bb";
 
     Stl s[2];
 
-    for (int i = 1; i < argc - 1; i++)
+    // 引数処理
+    folen = optprocess(argc, argv, shortotps, longopts, findopts, OPTSIZE);
+
+    for (int i = 0; i < folen; i++)
     {
-        // cout << "File: " << argv[i] << " -----" << endl;
-
-        if (s[i - 1].load(argv[i], srctype[i - 1] == 'b'))
+        switch (findopts[i].opt)
         {
-            cout << "Error: Failed to open a file: " << argv[i] << endl;
-            return -1;
+        case 'h':
+            printHelp();
+            return 0;
+        case 'v':
+            printVersion();
+            return 0;
+        case 'f':
+            srctype = findopts[i].arg;
+            break;
+        case 'r':
+            isShow = false;
+            break;
+        default:
+            break;
         }
-
-        // s[i - 1].show();
     }
 
-    if (s[0].equalsWithShow(s[1], true))
+    if (argc - optind != 2) // file1 file2
     {
-        cout << "[O] file structure matched." << endl;
+        cerr << "Error: Invlid argumanets." << endl;
+        return -1;
+    }
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (s[i].load(argv[optind + i], srctype[i] == 'b'))
+        {
+            cerr << "Error: Failed to open a file: " << argv[optind + i] << endl;
+            return -1;
+        }
+    }
+
+    if (isShow)
+    {
+        if (s[0].equalsWithShow(s[1], true))
+        {
+            cout << "[O] file structure matched." << endl;
+        }
+        else
+        {
+            cout << "[X] file structure unmatched." << endl;
+        }
     }
     else
     {
-        cout << "[X] file structure unmatched." << endl;
+        cout << (s[0] == s[1] ? "True" : "False") << endl;
     }
-    
+
     return 0;
+}
+
+void printHelp()
+{
+    cout << "Usage: stlc [options...] file1 file2" << endl;
+    cout << "Options:" << endl;
+    cout << "  -h          , --help                 Display this information." << endl;
+    cout << "  -v          , --version              Display command version information." << endl;
+    cout << "  -f<b/t...>  , --format=<b/t...> ," << endl;
+    cout << "  -f <b/t...> , --format <b/t...>      Specify the format of the file." << endl;
+    cout << "                                       Example: --format tb" << endl;
+    cout << "                                       It means file1->text, file2->binary." << endl;
+    cout << "  -r          , --result               Display only if there is a match. It returns \'True\' or \'False\'" << endl;
+    cout << endl;
+    cout << "For bug reporting instructions, please see:" << endl;
+    cout << "<" << ISSUE_LINK << ">." << endl;
+}
+
+void printVersion()
+{
+    cout << "stlc " << VERSION << endl;
+    cout << "Copyright (C) 2021 GrapeJuice." << endl;
 }
