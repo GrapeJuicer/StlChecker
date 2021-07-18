3a3c66c210d413b91a21ef4d3ff978a7b4074d50
GrapeJuicer <Dev.Grape@outlook.jp>
2021-07-18 20:58:45 +0900

    [update] Changed to use namespace to def constants

--- obj.hpp
+++ obj.hpp
@@ -6,6 +6,19 @@
 
 using namespace std;
 
+    namespace level
+    {
+        const int exactly = 0; // 完全一致. 頂点の順番が完全に一緒でないといけない
+        // const int pair    = 1; // n番目の要素が同じ面ならば頂点の順番が違っても許容する. 面の向き(法線)は一致する必要がある
+        // const int shape   = 2; // 最終的な形が同じならば許容．法線の向きは一致する必要がある．
+    }
+
+    namespace rule
+    {
+        const int direction = 0; // 点同士の方向別の距離で計算する(XYZのいずれかが範囲外なら範囲外)
+        const int point = 1;     // 点同士の直線距離から計算する
+    }
+
 class Vec3
 {
 private:
@@ -16,7 +29,7 @@ public:
     Vec3();
     Vec3(float x, float y, float z);
     ~Vec3();
-    float length();
+        bool inRange(const Vec3 &r, double range, int rule = stlset::rule::direction) const;
 
     // operator
     bool operator==(const Vec3 &r) const;
@@ -37,6 +50,7 @@ public:
     Face(Vec3 p1, Vec3 p2, Vec3 p3, Vec3 normal);
     Face(float x1, float y1, float z1, float x2, float y2, float z2, float x3, float y3, float z3, float nx, float ny, float nz);
     ~Face();
+        bool inRange(const Face &r, double range, int rule = stlset::rule::direction) const;
 
     // operator
     bool operator==(const Face &r) const;
@@ -57,16 +71,7 @@ public:
     string comment = "";
     vector<Face> faces;
 
-    enum PatternLevel
-    {
-        lv_exactly, // 完全一致. 頂点の順番が完全に一緒でないといけない
-        lv_pair,    // n番目の要素が同じ面ならば頂点の順番が違っても許容する. 面の向き(法線)は一致する必要がある
-        lv_shape,   // 最終的な形が同じならば許容．法線の向きは一致する必要がある．
-    };
-
-    // constructor/destructor
-    Stl();
-    ~Stl();
+        // constructor/destructor
 
     // method
     int size() const;
@@ -74,8 +79,9 @@ public:
     int load(ifstream &file, bool isBinary);
     void show() const;
     int showItem(int index) const;
-    bool equals(const Stl &r, enum PatternLevel lv = lv_exactly) const;
-    bool equalsWithShow(const Stl &r, bool isShow, enum PatternLevel lv = lv_exactly) const;
+        bool equals(const Stl &r, int lv = stlset::level::exactly) const;
+        bool equalsWithShow(const Stl &r, bool isShow, int lv = stlset::level::exactly) const;
+        bool inRangeWithShow(const Stl &r, float range, bool isShow, int rule = stlset::rule::direction) const;
 
     // operator
     bool operator==(const Stl &r) const;
