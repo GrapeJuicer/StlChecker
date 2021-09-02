e35af6ccae002e7741fe3547318ebac15e4eb5d6
GrapeJuicer <Dev.Grape@outlook.jp>
2021-09-03 04:42:10 +0900

    [update] add comment

--- main.cpp
+++ main.cpp
@@ -2,29 +2,38 @@
 #include "obj.hpp"
 #include "optarg.h"
 
+// オプションの種類の上限(現在は6)
 #define OPTSIZE 10
-
+// バージョン
 #define VERSION "1.0.0"
+// リンク
 #define ISSUE_LINK "https://github.com/GrapeJuicer/StlChecker/issues"
 
+// 名前空間
 using namespace std;
 using namespace stlset;
 
+// プロトタイプ宣言
 void printHelp();
 void printVersion();
 
+// グローバル変数
 bool isShow = true;
 
 int main(int argc, char *argv[])
 {
     /*
-    help
-    version
-    format
-    result
-    permission
+    h, help      ... ヘルプを表示
+    v, version   ... バージョンを表示
+    f, format    ... ファイルのフォーマットを指定
+    r, result    ... 結果(True/False)のみ表示
+    t, tolerance ... 許容誤差を設定
+    u, rule      ... 許容誤差設定時の誤差計算方式 (0,component:点の各成分の距離 / 1,vertex:点同士の距離)
     */
-    char shortotps[] = "hvf:rp:u:";
+
+    // オプション(短)
+    char shortotps[] = "hvf:rt:u:";
+    // オプション(長)
     struct option longopts[] = {
         {"help", no_argument, NULL, 'h'},
         {"version", no_argument, NULL, 'v'},
@@ -33,35 +42,40 @@ int main(int argc, char *argv[])
         {"permission", required_argument, NULL, 'p'},
         {"rule", required_argument, NULL, 'u'},
         OPT_END};
+    // 指定されたオプションの情報を格納する配列
     struct optarg findopts[OPTSIZE];
+    // 指定されたオプションの数
     int folen;
+    // ファイルのフォーマット (b:バイナリ / t:テキスト)
     string srctype = "bb";
-    double permission_range;
-    bool set_permission = false;
+    // 許容範囲
+    // 許容範囲設定フラグ
+    // 引数文字列処理用
     string opstr;
-    int rule = rule::direction;
-
+    // 計算方式
+    // 比較結果
     bool result;
-
+    // 引数で与えられた2つのSTLファイルのデータ
     Stl s[2];
 
     // 引数処理
     folen = optprocess(argc, argv, shortotps, longopts, findopts, OPTSIZE);
 
+    // 指定されたオプションの処理
     for (int i = 0; i < folen; i++)
     {
         switch (findopts[i].opt)
         {
-        case 'h':
+        case 'h': // help
             printHelp();
             return 0;
-        case 'v':
+        case 'v': // version
             printVersion();
             return 0;
-        case 'f':
+        case 'f': // format
             srctype = findopts[i].arg;
             break;
-        case 'r':
+        case 'r': // result
             isShow = false;
             break;
         case 'p':
@@ -89,18 +103,14 @@ int main(int argc, char *argv[])
         }
     }
 
+    // オプションを含めない引数の個数が2つで無い場合，エラーを出力して終了
     if (argc - optind != 2) // file1 file2
     {
         cerr << "Error: Invlid argumanets." << endl;
         return -1;
     }
 
-    if (set_permission && permission_range <= 0)
-    {
-        cerr << "Error: option.permission: Invalid value." << endl;
-        return -1;
-    }
-
+    // 引数で与えられた2つのファイルを読み込む
     for (int i = 0; i < 2; i++)
     {
         if (s[i].load(argv[optind + i], srctype[i] == 'b'))
@@ -110,8 +120,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    // check
-    if (set_permission)
+    // ファイル内容の比較
     {
         result = s[0].inRangeWithShow(s[1], permission_range, isShow, rule);
     }
@@ -119,8 +128,8 @@ int main(int argc, char *argv[])
     {
         result = s[0].equalsWithShow(s[1], isShow, level::exactly);
     }
-    
-    // show result
+
+    // 結果の表示
     if (isShow)
     {
         if (result)
@@ -140,6 +149,14 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+////////////////////////////////////////////////////////////////////////
+// overview:
+//      printHelp は，プログラムのヘルプを出力する関数である．
+// args:
+//      なし
+// return:
+//      なし
+////////////////////////////////////////////////////////////////////////
 void printHelp()
 {
     cout << "Usage: stlc [options...] file1 file2" << endl;
@@ -156,6 +173,14 @@ void printHelp()
     cout << "<" << ISSUE_LINK << ">." << endl;
 }
 
+////////////////////////////////////////////////////////////////////////
+// overview:
+//      printVersion は，プログラムのバージョンを出力する関数である．
+// args:
+//      なし
+// return:
+//      なし
+////////////////////////////////////////////////////////////////////////
 void printVersion()
 {
     cout << "stlc " << VERSION << endl;
